94e2580d1dd9607cc0453ca0ee59f46cf0337758
Presty12 <73420849+Presty12@users.noreply.github.com>
2021-01-17 08:24:22 +0000

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '800073584420454440', 
+  ownerRole:"800073584738435120", 
+  teyitsizRolleri:['790861621479211017'], 
+  jailRolu:"800073584671981583", 
+  enAltYetkiliRolu:['800073584612999244'], 
+  activity:"shina 💚 White", 
+  status:"online",
+  invitelink:"https://discord.gg/VzCMte5J4n", 
+  symbol:"❋", 
+  voicechannel:"800099227027505212" 
+};
+module.exports = ayarlar;
